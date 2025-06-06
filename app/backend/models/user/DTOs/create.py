55a3c38a76a321e355791d0f966610548e9d2ceb
Sergio Diez Fernández <145585172+SerDieFer@users.noreply.github.com>
2025-06-06 08:05:50 +0200

    fix:user_Service

--- app/backend/models/user/DTOs/create.py
+++ app/backend/models/user/DTOs/create.py
@@ -5,4 +5,4 @@ from backend.models.user.DTOs.base import UserBase
 
 # Definition of the UserCreate DTO model, which is used by the API endpoint to create a new user.
 class UserCreate(UserBase):
-    password: str = Field(..., min_length=6)      # Original password that will be encrypted before storing
+    password: str = Field(..., min_length=6)    # Original password that will be encrypted before storing
