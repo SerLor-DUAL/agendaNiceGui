d059e29fe0bbfbfdb52f48332bf618e54daa3b7e
Alejandro Crespo <crespoperezalex@gmail.com>
2025-06-09 17:48:57 +0200

    Header Links Component

--- app/frontend/routes/login.py
+++ app/frontend/routes/login.py
@@ -3,13 +3,12 @@
 from nicegui import ui
 from frontend.components.navbar import navbar
 from ..components.formCard import login_card
+from ..components.headerLinks import header_links
 # import httpx
 
 # Function to render the login page
 def create_login_page():
-    
-    # Custom CSS
-    ui.add_head_html('<link rel="stylesheet" href="/static/css/styleColores.css">')
+    header_links()
     
     # Login form
     with ui.column().classes('w-full h-screen bg-gray-100'):
--- app/frontend/routes/register.py
+++ app/frontend/routes/register.py
@@ -1,11 +1,12 @@
 from nicegui import ui
 from ..components.navbar import navbar
 from ..components.formCard import register_card
+from ..components.headerLinks import header_links
 
 def create_register_page():
-    # Agrega aquí el CSS personalizado
-    ui.add_head_html('<link rel="stylesheet" href="/static/css/styleColores.css">')
     
+    header_links()
+
     with ui.column().classes('w-full h-screen bg-gray-100'):
         navbar()
         register_card()
