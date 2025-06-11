21add733e0bb5066ad57d6c9b2e1baf7cde3221b
Santiago Lorenzano <enderwigim@gmail.com>
2025-06-11 16:51:18 +0200

    alex changes

--- app/frontend/routes/calendar.py
+++ app/frontend/routes/calendar.py
@@ -4,7 +4,7 @@ import httpx
 from ..components.navbar import navbar
 from ..components.headerLinks import header_links
 
-async def create_calendar_page():
+def create_calendar_page():
     # Check if the user is authenticated
 
     header_links()
