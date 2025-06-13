ffab6e347c0870fd5edf1b1327ca84adfa4f3df8
Santiago Lorenzano <enderwigim@gmail.com>
2025-06-13 14:47:01 +0200

    Revert "Update navbar.py"
    This reverts commit 08e543fe17325593ed17551b960b9ac8ddf3196d.

--- app/frontend/components/utils/navbar.py
+++ app/frontend/components/utils/navbar.py
@@ -1,20 +1,35 @@
 # frontend/components/navbar.py
 
-from nicegui import ui
-from fastapi.responses import RedirectResponse
-from frontend.components.utils.navbar_buttons import navbar_buttons
+# Import necesary modules
+from nicegui import ui                                                  # Import the ui module
+from frontend.components.utils.navbar_buttons import navbar_buttons     # Importing the navbar_buttons component
 
-# Cambia go_to para que retorne RedirectResponse
+# ----------------------------------------------------------------------------------------------------------------------------------------- #
+
 def go_to(route):
-    return RedirectResponse(url=route)
+    ui.navigate.to(route)
 
+# NOTE: Function to add the navbar component
 async def navbar():
+    
+    # Principal row
     with ui.row().classes('items-center justify-between bg-gray-800 p-4 text-white w-full rounded-t'):
+        
+        # Logo image
         img = ui.image('/static/img/logo.png').classes(
-            'w-16 h-auto rounded cursor-pointer hover:opacity-80 transition-opacity duration-300'
-        )
-        # Al hacer clic, la función go_to retorna RedirectResponse
-        img.on('click', lambda _: go_to('/'))
+                                                        'w-16 h-auto rounded '
+                                                        'cursor-pointer ' 
+                                                        'hover:opacity-80 '
+                                                        'transition-opacity duration-300' 
+                                                        )
+        
+        # Logo click event
+        img.on('click', go_to('/'))
+        
+        # Second row
+        with ui.row().classes('space-x-4'):
+            
+            # Add the buttons depending on the logged state
+            await navbar_buttons()
 
-        #with ui.row().classes('space-x-4'):
-            #await navbar_buttons()
+# ----------------------------------------------------------------------------------------------------------------------------------------- #
