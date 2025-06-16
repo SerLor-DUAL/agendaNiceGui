6f78df57b00e13d715b26bc4e68504c7a2d5fb44
Alejandro Crespo <crespoperezalex@gmail.com>
2025-06-16 17:34:27 +0200

    Design Responsive Menu

--- app/frontend/components/utils/navbar_links.py
+++ app/frontend/components/utils/navbar_links.py
@@ -19,10 +19,10 @@ desktop_link_classes = (
 # Styles for mobile links (botones que parecen links)
 mobile_link_classes = (
                             'text-white hover:text-blue-300 hover:bg-gray-800 '
-                            'text-lg px-4 py-4 rounded-lg '
+                            'text-lg px-4 py-1 rounded-lg '
                             'w-full text-left transition-all duration-200 '
                             'bg-transparent border-none font-medium '
-                            'flex items-center justify-start'
+                            'flex items-center justify-start no-underline'
                         )
             
 # NOTE: Function to add the navbar links component for desktop
@@ -41,10 +41,13 @@ async def navbar_links(current_user):
 async def mobile_nav_links(current_user):
     # logged = await auth.auth_required(check_only=True)
 
-    ui.button('Inicio', on_click=lambda: frh.go_to('/')).classes(mobile_link_classes)
+    # ui.button('Inicio', on_click=lambda: frh.go_to('/')).classes(mobile_link_classes)
+    ui.link('Inicio', '/').classes(mobile_link_classes)
 
     #If user is logged in show calendar and events links
     # if logged:
     if current_user:
-        ui.button('Calendario', on_click=lambda: frh.go_to('/calendar')).classes(mobile_link_classes)
-        ui.button('Eventos', on_click=lambda: frh.go_to('/events')).classes(mobile_link_classes)
+        # ui.button('Calendario', on_click=lambda: frh.go_to('/calendar')).classes(mobile_link_classes)
+        # ui.button('Eventos', on_click=lambda: frh.go_to('/events')).classes(mobile_link_classes)
+        ui.link('Calendario', '/calendar').classes(mobile_link_classes)
+        ui.link('Eventos', '/events').classes(mobile_link_classes)
