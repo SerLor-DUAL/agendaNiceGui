2197b447eb32981319a2a98b72e472a8b1685f96
Sergio Diez Fernández <145585172+SerDieFer@users.noreply.github.com>
2025-06-18 11:25:14 +0200

    updated home

--- app/frontend/routes/home.py
+++ app/frontend/routes/home.py
@@ -1,35 +1,31 @@
 # frontend/routes/home.py
 
 # Import necessary modules
-from nicegui import ui                                                  # Import the ui module
-from frontend.components.utils import navbar                            # Importing the utils components
-# Importing the AuthService instance
-from frontend.services.auth_services import front_auth_service as auth   
-# ----------------------------------------------------------------------------------------------------------------------------------------- #
+from nicegui import ui
 
 async def create_home_page():
-    """ Creates the home page """
-
-    with ui.row().classes(
-        'flex flex-col-reverse lg:flex-row items-start justify-end lg:justify-start w-full bg-gray-50 px-4 lg:px-8 py-8 ' +
-        'min-h-screen lg:min-h-0 lg:h-[80%]'
-    ):
-        # Left column
-        with ui.column().classes(
-            'w-full lg:flex-1 lg:h-full lg:pl-4 text-center lg:text-left lg:self-center'
+    """Creates the home page"""
+    
+    # Principal container
+    with ui.element('div').classes('grid grid-cols-1 lg:grid-cols-[1.1fr_0.9fr] xl:grid-cols-[0.8fr_1.2fr] gap-8 w-full md:h-[80vh] sm:h-[50vh] p-8 items-center md:pt-8'):
+        
+        # Text left column
+        with ui.element('div').classes(
+            'flex flex-col h-full justify-start items-center md:items-start text-center md:text-left order-2 md:order-1 md:pt-8'
         ):
             ui.label('AgendaNiceGUI').classes(
-                'text-4xl lg:text-6xl xl:text-7xl font-bold text-gray-900 mb-10 lg:mb-12 w-full slide-in-left'
-            ).style('animation-delay: 0s;')
-
-            ui.label('Esta agenda es nuestra brújula. 🧭').classes(
-                '!text-2xl lg:!text-3xl text-gray-700 w-full slide-in-left'
+                'text-2xl sm:text-3xl md:text-5xl font-bold text-gray-900 mb-6 slide-in-left'
             ).style('animation-delay: 0.1s;')
 
-        # Right column
-        with ui.column().classes(
-            'w-full lg:flex-1 pl-0 lg:pr-4 text-center lg:text-right'
+            ui.label('Esta agenda es nuestra brújula.🧭').classes(
+                'text-[14px] sm:text-xl md:text-2xl text-gray-700 slide-in-left'
+            ).style('animation-delay: 0.2s;')
+
+        # Image right column
+        with ui.element('div').classes(
+            'flex justify-center w-full h-full items-start order-1 md:order-2 md:pt-8'
         ):
-            ui.image('/static/img/hero.png').classes(
-                'w-[90%] h-auto p-4 mx-auto lg:mx-0 m-4 rounded-xl'
-            )
+            with ui.element('div').classes('w-full self-start'):
+                ui.image('/static/img/hero.png').classes(
+                    'w-full h-auto max-h-[40vh] md:max-h-[52vh] lg:max-h-[72vh] object-contain rounded-xl'
+                )
